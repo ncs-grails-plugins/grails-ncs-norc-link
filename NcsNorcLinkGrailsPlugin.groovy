454854015bf9ccaa4dc91c361ca30e0311903790
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-04-03 15:26:42 -0500

    bumping version to trigger a new release

--- NcsNorcLinkGrailsPlugin.groovy
+++ NcsNorcLinkGrailsPlugin.groovy
@@ -1,8 +1,8 @@
 class NcsNorcLinkGrailsPlugin {
     // the plugin version
-    def version = "0.5"
+    def version = "0.6"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.5 > *"
+    def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
     def dependsOn = [ ncsTracking : "3.2.6 > *", ncsPeople : "1.0 > *" ]
     // resources that are excluded from plugin packaging
